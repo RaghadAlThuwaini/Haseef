eb6189c2492a8fac2f6559430b3645ebc0cdfbce
hayfa <hyyofa@hotmail.com>
2021-02-11 04:34:08 +0300

    connect products page with the db + add package display products

---
+++ app/src/main/java/com/example/haseef4/displayProducts/product.java
@@ -0,0 +1,68 @@
+package com.example.haseef4.displayProducts;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.haseef4.R;
+import com.example.haseef4.StaffHome;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class product extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    ViewPagerAdapter viewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new productfragment1(), "Dairy products");
+        viewPagerAdapter.addFragment(new productfragment2(), "Juices");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ImageView back_icons = findViewById(R.id.back);
+
+//testsss
+        back_icons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(product.this, StaffHome.class));
+            }
+        });
+    }
+}
+
+//public class product extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_product);
+//        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+//        ViewPager viewPager = findViewById(R.id.view_pager);
+//        viewPager.setAdapter(sectionsPagerAdapter);
+//        TabLayout tabs = findViewById(R.id.tabs);
+//        tabs.setupWithViewPager(viewPager);
+
+//    }
+//
+//    }
--- app/src/main/java/com/example/haseef4/restockfragment1.java
+++ app/src/main/java/com/example/haseef4/restockfragment1.java
@@ -4,15 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.haseef4.R;
+import com.example.haseef4.displayProducts.productModel;
+import com.example.haseef4.displayProducts.product_adapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class restockfragment1 extends Fragment {
+//    ListView productList;
+//    DatabaseReference DPref;
+//    ArrayList<productModel> dairyPlist;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.restockfragment1_layout,container,false);
+//        productList = (ListView) view.findViewById(R.id.dairy_products_restock_view);
+//        dairyPlist = new ArrayList<>();
+//
+//        DPref = FirebaseDatabase.getInstance().getReference().child("products").child("dairyProducts");
+//        DPref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot productSnapshot: snapshot.getChildren()){
+//                    productModel P = productSnapshot.getValue(productModel.class);
+//                    dairyPlist.add(P);
+//                }
+//                product_adapter adapter = new product_adapter(getContext(), dairyPlist);
+//                productList.setAdapter(adapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        return view;
     }
 }
+
+
