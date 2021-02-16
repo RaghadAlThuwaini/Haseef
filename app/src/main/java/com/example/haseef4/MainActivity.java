a7b47a046395d3d19abaf7128bf74790a2a09819
raghad133 <58010294+raghad133@users.noreply.github.com>
2021-02-17 00:06:56 +0300

    test

--- app/src/main/java/com/example/haseef4/MainActivity.java
+++ app/src/main/java/com/example/haseef4/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//,,,,,
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
