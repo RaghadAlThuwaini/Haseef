8e07a6bd4a3f63a81d910229d28dfc8b33669f0a
raghad133 <58010294+raghad133@users.noreply.github.com>
2021-02-23 22:28:06 +0300

    test

--- app/src/main/java/com/example/haseef4/Login.java
+++ app/src/main/java/com/example/haseef4/Login.java
@@ -18,7 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+//....
 public class Login extends AppCompatActivity {
     EditText txtUser , txtPass;
     Button btnLogin;
