e0bb69439af84a2aee06ef51cb3936ed7bad6b6a
hayfa <hyyofa@hotmail.com>
2021-03-08 02:26:44 +0300

    add notification

---
+++ app/src/main/java/com/example/haseef4/notification.java
@@ -0,0 +1,36 @@
+package com.example.haseef4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.haseef4.displayProducts.productModel;
+import com.example.haseef4.displayProducts.product_adapter;
+
+import java.util.ArrayList;
+
+
+public class notification extends AppCompatActivity {
+    ListView list;
+    public static ArrayList<productModel> toNotifyProducts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        list = (ListView) findViewById(R.id.toNotify_listview);
+        product_adapter adapter = new product_adapter(getBaseContext(), toNotifyProducts);
+        list.setAdapter(adapter);
+
+        ImageView back_icons = findViewById(R.id.back);
+        back_icons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
